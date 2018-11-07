c809ff851a8e75e680398b944183910efb26a56c
Melissa Key West <art@mkaydub.com>
2018-11-07 12:30:51 -0700

    Imported SquareAPI and implemented the search parameter in order to find restaurants according to location and query.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 import MapContainer from './components/MapContainer.js'
+import SquareAPI from './API/'
 
 class App extends Component {
+	state = {
+		venues: [],
+		markers: [],
+		center: {},
+		zoom: 15,
+		map: null,
+		showingInfoWindow: false,
+		activeMarker: {},
+		selectedPlace: {},
+	}
+
+	componentDidMount() {
+		SquareAPI.search( {
+			near: 'Denver, CO',
+			limit: 50,
+			query: 'sushi'
+		} ).then( results => {
+			const { venues } = results.response;
+			const { center } = results.response.geocode.feature.geometry;
+			const { markers } = venues.map( venue => {
+				return {
+					lat: venue.location.lat,
+					lng: venue.location.lng,
+					isOpen: false,
+					isVisible: true
+				};
+			} );
+			this.setState( { venues, center, markers } );
+			for ( var i = 0; i < venues.length; i += 1 ) {
+				console.log( venues[ i ].location.lat, venues[ i ].location.lng );
+			}
+		} );
+	}
+
+
 	render() {
 		return (
 			<div className="App">
