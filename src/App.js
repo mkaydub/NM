0e9845ebd80bb7bd742405c914817730c9ff251e
Melissa Key West <art@mkaydub.com>
2018-11-07 12:27:33 -0700

    Brought map container into App.js and passed down the state

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 import MapContainer from './components/MapContainer.js'
 
 class App extends Component {
+	render() {
+		return (
+			<div className="App">
+        <div>
+					<h1>Denver, Colorado Foodie Scene</h1>
+        <MapContainer
+          {...this.state}/>
+        </div>
+
+
       </div>
-    );
-  }
+		);
+	}
 }
 
 export default App;
