66b08731c6cd614d9ce9b03f428c688886747fe2
Melissa Key West <art@mkaydub.com>
2018-11-07 12:29:43 -0700

    Created Maps Container, Marker, and info window. with google-maps-react added prop types, and exported.

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,4 +1,78 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import PropTypes from 'prop-types';
+
 const API_Key = 'AIzaSyDiWwXhqwiyeCoyClgDNrNbFmDNq5QwCrk'
+
+
+export class MapContainer extends Component {
+	state = {};
+
+	onMarkerClick = ( props, marker, e ) =>
+		this.setState( {
+			selectedPlace: props,
+			activeMarker: marker,
+			showingInfoWindow: true
+		} );
+
+	onMapClicked = ( props ) => {
+		if ( this.props.showingInfoWindow ) {
+			this.setState( {
+				showingInfoWindow: false,
+				activeMarker: null
+			} )
+		}
+	};
+
+	render() {
+		if ( !this.props.loaded ) {
+			return <div>Loading...</div>
+		}
+
+		const style = {
+			width: '100%',
+			height: '100%'
+		}
+		return (
+			<Map
+				{...this.props}
+				role='application'
+				aria-label='map'
+        google={this.props.google}
+        style={style}
+        onClick={this.closeInfoWindow}
+				center={this.props.center}>
+
+				{this.props.markers &&
+				this.props.markers
+					.filter(marker => this.props.markers)
+					.map( (marker,index) =>  (
+        		<Marker
+							position = {{lat:this.props.markers.forEach.lat, lng:this.props.markers.forEach.lng}}
+							key = {index}
+          		onClick={this.onMarkerClick}
+
+ 							/>
+								))}
+
+          <InfoWindow
+            marker={this.activeMarker}
+            onOpen={this.windowHasOpened}
+            onClose={this.windowHasClosed}
+            visible={this.showingInfoWindow}>
+              <div>
+                <h1>{this.props.selectedPlace.name}</h1>
+              </div>
+          </InfoWindow>
+      </Map>
+		);
+	}
+}
+MapContainer.propTypes = {
+	google: PropTypes.object,
+	zoom: PropTypes.number
+}
+
+export default GoogleApiWrapper( {
+	apiKey: API_Key
+} )( MapContainer )
