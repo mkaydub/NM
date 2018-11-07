af4ef88f7be955ba187c4777c44adeff19b50d63
Melissa Key West <art@mkaydub.com>
2018-11-07 12:25:37 -0700

    created api key variable

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,3 +1,4 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import PropTypes from 'prop-types';
+const API_Key = 'AIzaSyDiWwXhqwiyeCoyClgDNrNbFmDNq5QwCrk'
